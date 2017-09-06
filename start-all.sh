63781fe8d8ade746ca3f26a4072da584f3307257
Nipur Patodi <er.nipur.patodi@gmail.com>
2017-09-06 21:45:46 +0530

    Create start-all.sh

---
+++ start-all.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#--------------------------------------------------------#
+# @Author:       Nipur Patodi				 #
+# @Date:         06/09/2017				 #
+# @Description:  This Script is to restart all demoens.  #
+#--------------------------------------------------------#
+
+TOMCAT_HOME="/home/ubuntu/apache-tomcat-8.5.20"
+SPARK_HOME="/home/ubuntu/software/spark"
+KAFKA_HOME="/home/ubuntu/software/kafka"
+WEB_CODE="/home/ubuntu/code/web"
+BE_CODE="/home/ubuntu/code/be"
+IP="localhost"
+
+#Copying web app file to web app directory
+#Starting tomcat
+cp ${WEB_CODE}/*.war ${TOMCAT_HOME}/webapp
+${TOMCAT_HOME}/bin/startup.sh
+
+#Starting zookeeper and kafka
+${KAFKA_HOME}/bin/zookeeper-server-start.sh  ${KAFKA_HOME}/config/zookeeper.properties
+${KAFKA_HOME}/bin/kafka-server-start.sh  ${KAFKA_HOME}/config/server.properties
+
+#Starting spark
+${SPARK_HOME}/sbin/start-master.sh
+${SPARK_HOME}/sbin/start-slave.sh spark://${IP}:7077
+
+#Submitting Streaming job
+
+${SPARK_HOME}/bin/spark-submit --class org.dreambig.aad.xpression.Main --master spark://$IP:7077  --executor-memory 5G --total-executor-cores 3  ${BE_CODE}/Iexpress-all-1.0-SNAPSHOT.jar
+
+
+
+
+
