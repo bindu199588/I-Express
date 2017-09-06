19805d3db0ee9a9482cbded57b2cf11bfc19ab4e
Nipur Patodi <er.nipur.patodi@gmail.com>
2017-09-06 21:44:13 +0530

    Update SentimentAnalyser.scala

--- src/main/scala/org/dreambig/aad/xpression/extractor/utility/SentimentAnalyser.scala
+++ src/main/scala/org/dreambig/aad/xpression/extractor/utility/SentimentAnalyser.scala
@@ -12,14 +12,16 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
 import org.dreambig.aad.xpression.bean.{SentimentIdx, TagMessage}
+import org.dreambig.aad.xpression.extractor.utility.logging.InHouseLogging
 
 
 /**
   * Created by npatodi030716 on 3/2/2017.
   */
-object SentimentAnalyser {
+object SentimentAnalyser extends InHouseLogging{
 
   private val SentimentMap = Map(0 -> "upset", 1 -> "sad", 2 -> "neutral", 3 -> "happy", 4 -> "glad")
+  private val emoticonsMap= Map(":)"->3,":D"->4,":("->1,">:O"->0,":'("->0)
   private var pipeline: StanfordCoreNLP = null;
 
   private def getPipeLine: StanfordCoreNLP = {
@@ -36,17 +38,24 @@ object SentimentAnalyser {
     val annotation = pipe.process(msg)
     val sentences = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
     var maxSentSize = 0
-    var sentiment = -1
+    var sentiment:Float = 0
+
     for (i <- 0 to sentences.size() - 1) {
       val sentence = sentences.get(i)
-      val tree = sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])
-      val sc = RNNCoreAnnotations.getPredictedClass(tree)
-      if (sentence.toString.length > maxSentSize) {
-        maxSentSize = sentence.toString.length
-        sentiment = sc
+      if (emoticonsMap.contains(sentence.toString)){
+        sentiment +=emoticonsMap(sentence.toString)
+      }
+      else {
+
+        val tree = sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])
+        val sc = RNNCoreAnnotations.getPredictedClass(tree)
+
+        sentiment += sentimentCorrector(sentence.toString, sc)
+        log.info(s"${sentence.toString} => $sc + where as sentiment $sentiment")
       }
     }
-    sentiment
+
+    math.round(sentiment/sentences.size())
   }
 
 
@@ -60,8 +69,14 @@ object SentimentAnalyser {
 
   def analyze(rdd: RDD[TagMessage]): RDD[(String, SentimentIdx)] = {
 
-    rdd.map(x => (x.tagId, SentimentIdx(x.message, getSentiment( x.message))))
+    rdd.map(x => (x.tagId, SentimentIdx(x.message, getSentiment( x.message.trim))))
 
+  }
+
+  def sentimentCorrector(msg:String, sentiment:Int):Int={
+    if ((msg.toLowerCase.contains("don't") ||msg.toLowerCase.contains("do not")||msg.toLowerCase.contains("didn't")||msg.toLowerCase.contains("did not")) && sentiment<=2) sentiment
+    else if ((msg.toLowerCase.contains("like") ||msg.toLowerCase.contains("love") ) && sentiment<=2) 3
+    else sentiment
   }
 
 
